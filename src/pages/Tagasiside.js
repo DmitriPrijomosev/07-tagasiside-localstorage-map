bffe013dee37e6e6d52e63000f253ecf8ae4c38f
DmitriPrijomosev <dmitri@dezajno.ee>
2022-08-16 15:23:23 +0300

    07 kodukas
    map, splice, slice

---
+++ src/pages/Tagasiside.js
@@ -0,0 +1,33 @@
+import { useState, useRef } from "react";
+
+function Tagasiside() {
+
+	const [tagasisided, määraTagasisided] = useState(["Oli hea", "Huvitav", "Teistsugune", "Põnev"]);
+	const tagasisideRef = useRef();
+
+	function kustuta(index) {
+		console.log(index);
+		tagasisided.splice(index, 1);
+		määraTagasisided(tagasisided.slice());
+	}
+
+	function lisaUusTagasiside() {
+		tagasisided.push(tagasisideRef.current.value);
+		määraTagasisided(tagasisided.slice());
+	}
+
+	return (
+		<div>Tagasisided:
+			{tagasisided.map((element, index) =>
+			<div>
+				<span>{ element }</span>
+				<button onClick={() => kustuta(index)}>X</button>	
+			</div>)}
+			<br />
+			<label>Lisa uus tagasiside:</label>
+			<input ref={tagasisideRef} type="text"></input>
+			<button onClick={() => lisaUusTagasiside()}>Sisesta</button>
+		</div>);
+}
+
+export default Tagasiside;
