5081f8dd27bf93e29b3d8f8df73c78951f45fdc2
DmitriPrijomosev <dmitri@dezajno.ee>
2022-08-22 17:12:46 +0300

    parandatud

--- src/pages/TagasisideAndjad.js
+++ src/pages/TagasisideAndjad.js
@@ -1,42 +1,45 @@
 import { useState } from "react";
-import andjateFail from '../nimed.json';
+import andjateFail from "../nimed.json";
 
 function TagasisedeAndjad() {
-
   const [andjad, uuendaAndjad] = useState(andjateFail);
 
   const filtreeriM = () => {
-    const vastus = andjad.filter(element =>
-      element.startsWith('M'));
+    const vastus = andjad.filter((element) => element.startsWith("M"));
     uuendaAndjad(vastus);
-  }
+  };
 
   const filtreeri6 = () => {
-    const vastus6 = andjad.filter(element =>
-      element.length === 6);
+    const vastus6 = andjad.filter((element) => element.length === 6);
     uuendaAndjad(vastus6);
-  }
+  };
 
   const filtreeriY = () => {
     const vastusY = andjad.filter((element) =>
-      (element.slice(-1)).startsWith('y'));
+      element.slice(-1).startsWith("y")
+    );
     uuendaAndjad(vastusY);
-  }
+  };
 
   const filtreeriZA = () => {
     const vastusZA = andjad.sort().reverse();
     uuendaAndjad(vastusZA);
-  }
-
+  };
 
   return (
     <div>
       <div>Valitud kokku {andjad.length} nimesid</div>
-      <button onClick={() => filtreeriM()}>Filtreri ehk jätta kõik M tähega algavad nimed alles</button>
+      <button onClick={() => filtreeriM()}>
+        Filtreri ehk jätta kõik M tähega algavad nimed alles
+      </button>
       <br />
-      <button onClick={() => filtreeri6()}>Filtreeri ehk jätta kõik täpselt 6 kohalised nimed alles</button>
+      <button onClick={() => filtreeri6()}>
+        Filtreeri ehk jätta kõik täpselt 6 kohalised nimed alles
+      </button>
       <br />
-      <button onClick={() => filtreeriY()}>Filtreeri ehk jätta kõik Y tähega lõpevad nimed alles</button>
+      <button onClick={() => filtreeriY()}>
+        Filtreeri ehk jätta kõik Y tähega lõpevad nimed alles
+      </button>
       <br />
       <button onClick={() => filtreeriZA()}>Sorteeri Z-A</button>
       <br />
@@ -46,9 +49,13 @@ function TagasisedeAndjad() {
         Leia üles mis on nime Paul järjekorranumber - console või HTML
       </button>
       <br />
-      <button>Kuva tähestikus esimene, kellel on D tähega algav nimi. Console või HTML</button>
+      <button>
+        Kuva tähestikus esimene, kellel on D tähega algav nimi. Console või HTML
+      </button>
       <br />
-      {andjad.map((element) => <div>{element}</div>)}
+      {andjad.map( element => 
+        <div key={element}>{element}</div>
+      )}
     </div>
   );
 }
